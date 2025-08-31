0293ccc253a831e7f1f5f777eca9f5fef8e0b99c
Geofrey Simiyu <106971868+Geoffy1@users.noreply.github.com>
2025-08-31 22:44:01 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,14 +1,19 @@
-# Create your views here.
-from django.shortcuts import render
-from django.core.cache import cache
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse
 from .utils import get_all_properties
 
-#@cache_page(60 * 15) ,commented out as we're now handling caching manually
 def property_list(request):
-    """
-    A view to display a list of all properties.
-    """
-    properties = Property.objects.all()
-    return render(request, 'properties/property_list.html', {'properties': properties})
+    properties = get_all_properties()
+    return JsonResponse({"data": properties})
+
+
+@cache_page(60 * 15)  # cache for 15 minutes
+def property_list(request):
+    properties = Property.objects.all().values("id", "title", "description", "price", "location", "created_at")
+    return JsonResponse({"data": list(properties)})
+
+def cache_metrics(request):
+    metrics = get_redis_cache_metrics()
+    return JsonResponse(metrics)
